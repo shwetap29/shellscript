d0a4216aad2cfb229cfbe128a2eacbe244074981
shweta prasad <42508166+shwetap29@users.noreply.github.com>
2021-05-16 21:40:51 +0530

    Temperature conversion by the help of case statement

--- ProgramRepetition functionproblems/Tempconversion.sh
+++ ProgramRepetition functionproblems/Tempconversion.sh
@@ -1,1 +1,37 @@
+#! /bin/bash -x
+function  CelciusToFarenheit()
+{
+      read -p "Enter celcius temperature:" CelciusTemp
+      if(( $((CelciusTemp>=0 && CelciusTemp<=100 )) ))
+      then
+          Farenheit=`echo "scale=4;(($CelciusTemp*9/5))+32" | bc -1`
+          echo "Temperature in farenhit is:"`echo "scale=3; $farenhit" | bc -1`
+      else
+           echo "Enter between 0 to 100 Celcius temperature"
+       fi
+}
 
+function FarenheitToCelcius() 
+{
+ read -p "Enter Farenheit temperature:" FarenheitTemp
+      if(( $((FarenheitTemp>=32 && FarenheitTemp<=100 )) ))
+      then
+          Celcius=`echo "scale=4;(($FarenheitTemp-32))*5/9" | bc -1`
+           echo"Temperature in Celcius is:"`echo "scale=3; $Celcius" | bc- 1`
+      else
+           echo "Enter between 32 to 212 Farenheit temperature"
+       fi
+}
+echo "1.Celcius to Frenheit"
+echo "2.Farenheit to Celsius"
+read -p "Enter number:" user_input
+case $user_input in
+     1)
+  CelsiusToFarenheit
+     ;;
+     2) 
+  FarenheitTocelsius
+     ;;
+     *)
+     echo "Invalid Input"
+esac
